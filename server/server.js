446ad1d4ec9a9d84888a9845c238f31d2c5a42f2
Justin Cox <justincox@Violmusio.attlocal.net>
2019-02-06 11:52:39 -0600

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -18,9 +18,9 @@ app.post('/todos', (req, res) => {
   });
 
   todo.save().then((doc) => {
-    res.status(200).send(doc);
+    return res.status(200).send(doc);
   }, (err) => {
-    res.status(400).send(err);
+    return res.status(400).send(err);
   });
 });
 
@@ -28,7 +28,7 @@ app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (err) => {
-    res.status(400).send(err);
+    return res.status(400).send(err);
   });
 });
 
@@ -37,20 +37,20 @@ app.get('/todos/:id', (req, res) => {
 
   // Check for valid id format first
   if (!ObjectID.isValid(id)) {
-    res.status(404).send();
+    return res.status(404).send();
   }
 
   // If valid id, query DB for todo and return or return 404
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      res.status(404).send();
+      return res.status(404).send();
     }
-    res.status(200).send({todo});
+    return res.status(200).send({todo});
   }, (err) => {
-    res.status(404).send(`ERROR: No todo found with the id: ${id}.\n`, err);
+    return res.status(404).send(`ERROR: No todo found with the id: ${id}.\n`, err);
   }).catch((err) => {
     // Catch and return any other errors
-    res.status(400).send(err);
+    return res.status(400).send(err);
   });
 });
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,5 +1,6 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('../server');
 const { Todo } = require('../models/Todo');
@@ -7,8 +8,8 @@ const { Todo } = require('../models/Todo');
 
 // Create array of 'dummy' todos
 const todos = [
-  { text: 'First test todo' },
-  { text: 'Second test todo' }
+  { _id: new ObjectID(), text: 'First test todo' },
+  { _id: new ObjectID(), text: 'Second test todo' }
 ];
 
 // Clear the database before each test
@@ -70,4 +71,33 @@ describe('GET /todos', () => {
       })
       .end(done);
   });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    // make sure you get a 404 back
+    let fakeId = new ObjectID().toHexString();
+    
+    request(app)
+      .get(`/todos/${fakeId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object Ids', (done) => {
+    request(app)
+      .get(`/todos/123abc`)
+      .expect(404)
+      .end(done);
+  })
 });
