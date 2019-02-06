cc95bbe4c75dfca500df17ce59d65212254e6fd4
Justin Cox <justincox@Violmusio.attlocal.net>
2019-02-06 10:27:22 -0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { User } = require('./models/User');
@@ -31,6 +32,27 @@ app.get('/todos', (req, res) => {
   });
 });
 
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  // Check for valid id format first
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  // If valid id, query DB for todo and return or return 404
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }, (err) => {
+    res.status(404).send(`ERROR: No todo found with the id: ${id}.\n`, err);
+  }).catch((err) => {
+    // Catch and return any other errors
+    res.status(400).send(err);
+  });
+});
 
 // LISTEN
 app.listen(3000, () => {
