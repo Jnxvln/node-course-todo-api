fd73c22fa94729c74a3d5d4edbbd06ea4855f029
Justin Cox <justincox@Violmusio.attlocal.net>
2019-02-07 15:24:54 -0600

    Add POST /users/login route

--- server/models/User.js
+++ server/models/User.js
@@ -69,6 +69,21 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  let User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return bcrypt.compare(password, user.password).then(res => {
+      if (res) return user
+      else return Promise.reject();
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   let user = this;
   if (user.isModified('password')) {
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const bcrypt = require('bcryptjs');
 const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
@@ -132,6 +133,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((err) => {
+    res.status(400).send();
+  });
 });
 
 // LISTEN
